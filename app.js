a9b9ae9edd584882c64050e21749e76ca91914b3
Paweł Kozłowski <152084345+sinet77@users.noreply.github.com>
2024-06-03 08:42:32 +0000

    photo zoom by clicking on it

---
+++ app.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('container')
+const imageOnClick = document.querySelector('.clickable-image')
+const newBox = document.querySelector('.newBox')
+const close = document.querySelector('.close')
+
+
+imageOnClick.addEventListener('click', function(e) {
+    const tgt = e.target;
+    tgt.classList.add('zoomed')
+    newBox.classList.remove('hidden')
+    newBox.appendChild(imageOnClick)
+  })
+
+  close.addEventListener('click', function() {
+    newBox.classList.add('hidden');
+    imageOnClick.classList.remove('hidden');
+    imageOnClick.classList.remove('zoomed')
+    container.appendChild(imageOnClick);
+});
+
+imageOnClick.addEventListener('mousemove',function(){
+    
+})
