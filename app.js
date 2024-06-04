0539b36bf421f2e23ab46a17a084dded50c57352
Paweł Kozłowski <152084345+sinet77@users.noreply.github.com>
2024-06-04 13:25:50 +0000

    zoom on mouse move

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const container = document.getElementById('container')
 const imageOnClick = document.querySelector('.clickable-image')
 const newBox = document.querySelector('.newBox')
-const close = document.querySelector('.close')
+const closeButton = document.querySelector('.close')
+let isZoomed = false;
 
 
 imageOnClick.addEventListener('click', function(e) {
@@ -9,15 +10,30 @@ imageOnClick.addEventListener('click', function(e) {
     tgt.classList.add('zoomed')
     newBox.classList.remove('hidden')
     newBox.appendChild(imageOnClick)
+    isZoomed = true;
   })
 
-  close.addEventListener('click', function() {
+  closeButton.addEventListener('click', function() {
     newBox.classList.add('hidden');
     imageOnClick.classList.remove('hidden');
     imageOnClick.classList.remove('zoomed')
     container.appendChild(imageOnClick);
+    isZoomed=false;
 });
 
-imageOnClick.addEventListener('mouseenter',function(e){
-  this.style.backgroundSize = "250%";
+imageOnClick.addEventListener('mousemove', function(e) {
+  if (!isZoomed) return;
+  // Calculate the position of the cursor inside the element (in pixels).
+  let x = e.clientX - e.target.offsetLeft;
+  let y = e.clientY - e.target.offsetTop;
+
+  console.log(x,y)
+ 
+  imageOnClick.style.transformOrigin = `${x}px ${y}px`;
+  imageOnClick.style.transform = "scale(3)"
 })
+imageOnClick.addEventListener('mouseleave', function() {
+  if (!isZoomed) return;
+  imageOnClick.style.transformOrigin = "center";
+  imageOnClick.style.transform = "scale(1)"
+ });
